89835eb61016862ecc816650f7fed25583a597de
Edun Omatseye <edunomatseye@yahoo.com>
2024-09-20 07:34:14 +0100

    wip add test for division function

--- src/utils.ts
+++ src/utils.ts
@@ -11,5 +11,8 @@ export function multiply(a: number, b: number): number {
 }
 
 export function divide(a: number, b: number): number {
+  if (b === 0) {
+    throw new Error('Division by zero is not allowed');
+  }
   return a / b;
 }
